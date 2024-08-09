ee3754066ce354919cec3ad72420ef72cd2138cf
Ankit Saraswat <152775694+saraswatAnkit@users.noreply.github.com>
2024-08-09 23:03:12 +0530

    first commit

---
+++ codyankit.js
@@ -0,0 +1,6 @@
+const typed1 = new Typed('.type-effect1', {
+    strings: ['Everthing Is goona be alright. Krishna Ji is with u ,He will never let u down. You are gonna get well and Fit. I dont know how to tell u that u are a good person. Nothing is going to happen with u. See I always be there for you'],
+    typeSpeed: 10,
+    backSpeed: 40,
+    loop: true,
+});
